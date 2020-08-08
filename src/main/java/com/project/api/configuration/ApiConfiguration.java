a772731b38c73a0d8c31ce1ff4e188ade80e21f3
Adam <a20152-1@yahoo.com>
2020-08-08 15:25:43 -0400

    Remove circular dependency

--- src/main/java/com/project/api/configuration/ApiConfiguration.java
+++ src/main/java/com/project/api/configuration/ApiConfiguration.java
@@ -1,17 +1,10 @@
 package com.project.api.configuration;
 
-import com.project.api.entity.InventoryItem;
 import com.project.api.handler.InventoryHandler;
 import com.project.api.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisOperations;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -36,17 +29,4 @@ public class ApiConfiguration {
                 .andRoute(PUT("/inventory").and(accept(APPLICATION_JSON)), inventoryHandler::inventoryUpdate)
                 .andRoute(DELETE("/inventory/{id}").and(accept(APPLICATION_JSON)), inventoryHandler::inventoryDelete);
     }
-
-    @Bean
-    ReactiveRedisOperations<String, InventoryItem> redisOperations(ReactiveRedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<InventoryItem> serializer = new Jackson2JsonRedisSerializer<>(InventoryItem.class);
-
-        RedisSerializationContext.RedisSerializationContextBuilder<String, InventoryItem> builder =
-                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
-
-        RedisSerializationContext<String, InventoryItem> context = builder.value(serializer).build();
-
-        return new ReactiveRedisTemplate<>(factory, context);
-    }
-
 }
--- src/main/java/com/project/api/service/InventoryService.java
+++ src/main/java/com/project/api/service/InventoryService.java
@@ -1,15 +1,29 @@
 package com.project.api.service;
 
 import com.project.api.entity.InventoryItem;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
-@RequiredArgsConstructor
 public class InventoryService {
     private final ReactiveRedisOperations<String, InventoryItem> inventoryItemReactiveRedisOperations;
+
+    public InventoryService(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<InventoryItem> serializer = new Jackson2JsonRedisSerializer<>(InventoryItem.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, InventoryItem> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, InventoryItem> context = builder.value(serializer).build();
+
+         inventoryItemReactiveRedisOperations = new ReactiveRedisTemplate<>(factory, context);
+    }
 
     public Flux<InventoryItem> get(String id) {
         return inventoryItemReactiveRedisOperations.keys(id)
