5bd59c7e89c8371c3aca07c5e0faf175fb0c51cd
Adam <a20152-1@yahoo.com>
2020-11-08 23:04:29 -0500

    Create WebConfiguration.java

---
+++ src/main/java/com/project/api/configuration/WebConfiguration.java
@@ -0,0 +1,23 @@
+package com.project.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("cors")
+public class WebConfiguration {
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("localhost", "mytrustedwebsite.com"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
